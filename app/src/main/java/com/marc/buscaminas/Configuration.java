b1e94ce64ca4a4d03a2576728de324f110633aa4
joanss2 <64496738+joanss2@users.noreply.github.com>
2021-05-03 19:08:49 +0200

    StartCode Configuration

--- app/src/main/java/com/marc/buscaminas/Configuration.java
+++ app/src/main/java/com/marc/buscaminas/Configuration.java
@@ -1,15 +1,32 @@
 package com.marc.buscaminas;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Configuration extends AppCompatActivity {
 
+    private ImageButton backToMain;
+    private Button startGame;
+    private EditText userName;
+    private RadioGroup radioGropuNumeroParrilla, radioGroupBombsPercentage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.configuration);
+
+        backToMain = (ImageButton) findViewById(R.id.ConfigBackToMain);
+        startGame = (Button) findViewById(R.id.EmpezarDesdeConfig);
+        userName = findViewById(R.id.EditText_username);
+        radioGropuNumeroParrilla = findViewById(R.id.RadioGroupGraella);
+        radioGroupBombsPercentage = findViewById(R.id.RadioGroupBombs);
 
     }
 
---
+++ app/src/main/java/com/marc/buscaminas/Partida.java
@@ -0,0 +1,14 @@
+package com.marc.buscaminas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Partida extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.partida);
+    }
+}
