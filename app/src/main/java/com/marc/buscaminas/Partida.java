c6ad0fb18e7b8162e40c1b91ce3f6fb94b9343d4
joanss2 <64496738+joanss2@users.noreply.github.com>
2021-05-06 18:22:29 +0200

    inici grid( de moment esta mal)

--- app/src/main/java/com/marc/buscaminas/Partida.java
+++ app/src/main/java/com/marc/buscaminas/Partida.java
@@ -2,17 +2,30 @@ package com.marc.buscaminas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import org.w3c.dom.Text;
+
+import java.util.List;
 
 public class Partida extends AppCompatActivity {
 
     private Intent receivedIntent;
     private DadesDePartida receivedData;
+    private GridView graella;
+    private List<Button> cells;
 
 
     @Override
@@ -23,12 +36,46 @@ public class Partida extends AppCompatActivity {
         receivedIntent = getIntent();
         receivedData = receivedIntent.getExtras().getParcelable("DadesDePartida");
 
-        Toast.makeText(this,String.valueOf(receivedData.getNumero_graella()),Toast.LENGTH_SHORT).show();
-        Toast.makeText(this,String.valueOf(receivedData.getPercentatge()),Toast.LENGTH_SHORT).show();
+        ////////VIDEO
+        graella = (GridView) findViewById(R.id.gridviewID);
+        CustomAdapter gridAdapter = new CustomAdapter();
+        graella.setAdapter(gridAdapter);
+        graella.setNumColumns(receivedData.getNumero_graella());
 
         TextView user = (TextView) findViewById(R.id.userNameEnLaPartida);
         user.setText(receivedIntent.getExtras().getString("userName"));
 
 
     }
+
+    private class CustomAdapter extends BaseAdapter {
+        private Context context;
+        private Button cells;
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            View view1 = getLayoutInflater().inflate(R.layout.partida,null);
+             = view1.findViewById(R.id.cellButton);
+
+
+        }
+    }
+
+
+
 }
